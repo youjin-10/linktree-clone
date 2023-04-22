4c71720de5da7e2953231ece294e7d8cd104a222
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-04-22 22:40:46 +0900

    design: gradient bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="from-gray-900 to-gray-600 bg-gradient-to-r">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
